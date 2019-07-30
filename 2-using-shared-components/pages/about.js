c73c4d3d5b984df609d0c3d719748d152422b706
Carolina Dávila <cdavila19@gmail.com>
2019-07-30 17:15:38 -0500

    Se agregan pruebas con NEXT.js 	modificado:     1-navigate-between-pages/pages/index.js 	nuevo archivo:  2-using-shared-components/components/Header.js 	nuevo archivo:  2-using-shared-components/components/MyLayout.js 	nuevo archivo:  2-using-shared-components/components/MyLayout2.js 	modificado:     2-using-shared-components/pages/about.js 	modificado:     2-using-shared-components/pages/index.js 	copiado:        4-clean-urls/pages/index.js -> 3-create-dynamic-pages/pages/index.js 	nuevo archivo:  3-create-dynamic-pages/pages/post.js 	modificado:     4-clean-urls/pages/index.js 	nuevo archivo:  4-clean-urls/pages/p/[id].js

--- 2-using-shared-components/pages/about.js
+++ 2-using-shared-components/pages/about.js
@@ -1,7 +1,13 @@
-export default function About() {
-  return (
-    <div>
-      <p>This is the about page</p>
-    </div>
-  )
+//Method #1
+/* import withLayout from '../components/MyLayout'
+const Page = () => <p>This is the about page</p>
+export default withLayout(Page) */
+
+//Method #2
+import Layout from '../components/MyLayout2'
+
+const aboutPageContent = <p>This is about page Layout #2</p>
+
+export default function Index() {
+  return <Layout content={aboutPageContent} />
 }
--- 2-using-shared-components/pages/index.js
+++ 2-using-shared-components/pages/index.js
@@ -1,12 +1,13 @@
-import Link from 'next/link'
+//Ejemplo con Hight Order Component
+/* import withLayout from '../components/MyLayout'
+const Page = () => <p>Hello Next.js</p>
+export default withLayout(Page) */
+
+//Ejemplo con Page content as a prop
+import Layout from '../components/MyLayout2'
+
+const indexPageContent = <p>Hello Next.js Layout #2</p>
 
 export default function Index() {
-  return (
-    <div>
-      <Link href="/about">
-        <a>About Page</a>
-      </Link>
-      <p>Hello Next.js</p>
-    </div>
-  )
+  return <Layout content={indexPageContent} />
 }
